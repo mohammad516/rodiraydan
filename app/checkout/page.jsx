3b966a69eefbaab1750153ea4c882447f8baf0fe
Mohamad Rkein <mohammadrkein79@gmail.com>
2024-05-07 16:42:21 +0300

    first commit

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,23 +1,35 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './_components/CheckoutForm'
+import CheckoutForm from './_components/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
+
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
 function Checkout() {
-	const searchParams = useSearchParams();
-	const options = {
-		mode: 'payment',
-		currency: 'usd',
-		amount: searchParams.get('amount') * 100
-	}
-	return (
-		<Elements stripe={stripePromise} options={options}>
-			<CheckoutForm amount={Number(searchParams.get('amount'))} />
-		</Elements>
-	)
+  // Wrap the client-side code with Suspense and provide a fallback
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ClientSideCheckout />
+    </Suspense>
+  );
 }
 
-export default Checkout
+// Move the client-side code to a separate component
+function ClientSideCheckout() {
+  const searchParams = useSearchParams();
+  const options = {
+    mode: 'payment',
+    currency: 'usd',
+    amount: searchParams.get('amount') * 100,
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={Number(searchParams.get('amount'))} />
+    </Elements>
+  );
+}
+
+export default Checkout;
