d1a24a54b9d2e1dabb576072f6657465cf28c1d7
Mohamad Rkein <mohammadrkein79@gmail.com>
2024-05-08 17:52:02 +0300

    fix

--- app/_components/Subscribe.jsx
+++ app/_components/Subscribe.jsx
@@ -5,7 +5,7 @@ function Subscribe() {
     <section className="subscribe_section">
   <div className="tw-flex  tw-items-center tw-justify-center tw-bg-gradient-to-tr tw-bg-gray-100 tw-from-gray-600 tw-p-20">
   <div className="tw-w-max">
-    <h1 className="tw-animate-typing tw-overflow-hidden tw-whitespace-nowrap tw-border-r-4 tw-border-r-white tw-pr-5 tw-text-5xl tw-text-white tw-font-bold">
+    <h1 className="tw-animate-typing tw-overflow-hidden tw-whitespace-nowrap tw-border-r-4 tw-border-r-white tw-pr-5 tw-text-4xl sm:tw-text-6xl tw-text-white tw-font-bold">
     Приятных покупок!
     </h1>
   </div>
